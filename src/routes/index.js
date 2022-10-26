b33be8880d89e56490fdb51910096cbd16a61268
adriantintpilver <adriantintpilver@gmail.com>
2022-10-26 15:08:27 -0300

    stop animations and add search functionality
    stop animations and add search functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,12 +9,34 @@ const Image = require('../models/Image');
 const { Console } = require('console');
 
 router.get('/', async (req, res) => {
+    const { find } = "";
     const images = await Image.find();
-    res.render('index', { images });
+    res.render('index', { images, find });
+});
+
+router.get('/find', async (req, res) => {
+    const { find } = "";
+    const images = await Image.find();
+    res.render('index', { images, find });
+});
+
+router.post('/find', async (req, res) => {
+    const find = req.body.search;
+    const query = {$or:[{"title": { $regex: '.*' + find + '.*' }}, {"description": { $regex: '.*' + find + '.*' }}]};
+    const imagesFind = await Image.find(query);
+    res.render('search', { imagesFind, find });
+});
+
+router.get('/find/:find', async (req, res) => {
+    const { find } = req.params;
+    const query = {$or:[{"title": { $regex: '.*' + find + '.*' }}, {"description": { $regex: '.*' + find + '.*' }}]};
+    const imagesFind = await Image.find(query);
+    res.render('search', { imagesFind, find });
 });
 
 router.get('/upload', (req, res) => {
-    res.render('upload');
+    const { find } = req.params;
+    res.render('upload',{ find });
 });
 
 router.post('/upload', async (req, res) => {
@@ -33,15 +55,27 @@ router.post('/upload', async (req, res) => {
 
 router.get('/image/:id', async (req, res) => {
     const { id } = req.params;
+    const find = "";
     const image = await Image.findById(id);
-    res.render('profile', { image });
+    res.render('profile', { image, find });
 });
 
-router.get('/image/:id/delete', async (req, res) => {
-    const { id } = req.params;
+router.get('/image/:id/:find', async (req, res) => {
+    const id = req.params.id;
+    const find = req.params.find;
+    const image = await Image.findById(id);
+    res.render('profile', { image, find });
+});
+
+router.get('/image/:id/delete/ok', async (req, res) => {
+    const id = req.params.id;
     const imageDeleted = await Image.findByIdAndDelete(id);
     await unlink(path.resolve('./src/public' + imageDeleted.path));
     res.redirect('/');
+});
+
+router.get('/test', async (req, res) => {
+    res.render('testmenu', {});
 });
 
 module.exports = router;
